ace0b0ddf45107854de572eae5e915640495b59a
coutinho <segunademola313@gmail.com>
2025-03-27 05:59:33 +0100

    Project Organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,16 +1,5 @@
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+import Counter from "@/app/_components/Counter";
 
-  console.log(data);
-  return (
-    <div>
-      <h1>Cabins Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+export default function Page() {
+  return <div></div>;
 }
