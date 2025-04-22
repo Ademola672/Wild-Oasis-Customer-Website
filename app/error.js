0b3327f0d6aae5e50a0a8a265e6bcca4cea82da0
coutinho <segunademola313@gmail.com>
2025-04-22 23:45:10 +0100

    Error Handling: Setting Up Error Boundaries..

--- app/error.js
+++ app/error.js
@@ -10,7 +10,7 @@ export default function Error({ error, reset }) {
         className="inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg"
         onClick={reset}
       >
-        Try again
+        Try again ...
       </button>
     </main>
   );
